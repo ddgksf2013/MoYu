1635e6e0139afbe60d28dbe3144d89480a2eff99
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-04-05 20:32:52 +0800

    Update apowersoft.js

--- apowersoft.js
+++ apowersoft.js
@@ -21,24 +21,8 @@ hostname=awvp.aoscdn.com
 
 ***********************************/
 
-var ddgksf2013={
-  "status" : 200,
-  "message" : "success",
-  "data" : {
-    "group_expired_at" : 0,
-    "is_tried" : 0,
-    "max_devices" : 1,
-    "period_type" : "active",
-    "candy_expired_at" : 0,
-    "pending" : 0,
-    "remained_seconds" : 0,
-    "limit" : 0,
-    "expired_at" : 4045798296,
-    "candy" : 0,
-    "license_type" : "premium",
-    "quota" : 0,
-    "status" : 1,
-    "coin" : 100
-  }
-}
-$done({body: JSON.stringify(ddgksf2013)});
+
+
+
+
+var ddgksf2013={status:200,message:"success",data:{group_expired_at:0,is_tried:0,max_devices:1,period_type:"active",candy_expired_at:0,pending:0,remained_seconds:0,limit:0,expired_at:4045798296,candy:0,license_type:"premium",quota:0,status:1,coin:100}};$done({body:JSON.stringify(ddgksf2013)});
