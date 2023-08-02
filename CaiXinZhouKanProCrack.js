1ef589f7e2fec7f3eb953c97d8ce2fe95a86c57f
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-08-02 22:41:54 +0800

    Create CaiXinZhouKanProCrack.js

---
+++ CaiXinZhouKanProCrack.js
@@ -0,0 +1,35 @@
+/***********************************
+
+> 应用名称：财新周刊
+> 软件版本：4.0.6
+> 脚本作者：ddgksf2013
+> 微信账号：墨鱼手记
+> 解锁说明：解锁文章阅读权限
+> 更新时间：2023-08-02
+> 通知频道：https://t.me/ddgksf2021
+> 贡献投稿：https://t.me/ddgksf2013_bot
+> 问题反馈：ddgksf2013@163.com
+> 特别提醒：如需转载请注明出处，谢谢合作！
+> 特别说明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载售卖
+          ⚠️⚠️⚠️
+
+
+[rewrite_local]
+  
+# > 财新周刊☆解锁阅读权限（2023-08-02）@ddgksf2013
+https?:\/\/ipadcms\.caixin\.com\/tmp\/articles url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/CaiXinZhouKanProCrack.js
+
+
+[mitm] 
+
+hostname=ipadcms.caixin.com
+
+***********************************/
+
+
+
+
+
+var body = $response.body.replace(/isfree":0/g, 'isfree":1')
+$done({ body })
