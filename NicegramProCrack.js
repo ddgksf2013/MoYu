1f979c6446a8857d5f03960d06c9413409618298
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-08-19 08:07:58 +0800

    Create NicegramProCrack.js

---
+++ NicegramProCrack.js
@@ -0,0 +1,30 @@
+/***********************************
+
+> 应用名称：Nicegram
+> 软件版本：1.3.8
+> 脚本作者：ddgksf2013
+> 微信账号：墨鱼手记
+> 更新时间：2023-08-19
+> 通知频道：https://t.me/ddgksf2021
+> 贡献投稿：https://t.me/ddgksf2013_bot
+> 问题反馈：ddgksf2013@163.com
+> 特别提醒：如需转载请注明出处，谢谢合作！
+> 特别说明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载售卖
+          ⚠️⚠️⚠️
+
+
+[rewrite_local]
+  
+# > Nicegram☆解锁会员权限（2023-08-19）@ddgksf2013
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://github.com/ddgksf2013/dev/raw/main/NicegramProCrack.js
+
+
+[mitm] 
+
+hostname=restore-access.indream.app
+
+***********************************/
+
+
+
