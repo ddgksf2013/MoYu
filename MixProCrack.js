7ca6a5b2e51d0579811190aaf3d0ae7b33e87f32
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-10-06 15:57:42 +0800

    Update MixProCrack.js

--- MixProCrack.js
+++ MixProCrack.js
@@ -34,9 +34,5 @@ hostname=cdn-bm.camera360.com
 
 let body= $response.body;
 var obj = JSON.parse(body);
-if (body.indexOf("expires") !=-1) {;
-  obj["data"]["orderList"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
-  obj["data"]["orderList"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
-  obj["data"]["orderList"][0]["expires_date_ms"] = "4096019658000";
-  }
+obj.data.orderList = [{"expires_date":"2099-10-19 05:14:18 Etc/GMT","expires_date_pst":"2099-10-18 22:14:18 America/Los_Angeles","expires_date_ms":"4096019658000"}];
 $done({body: JSON.stringify(obj)});
