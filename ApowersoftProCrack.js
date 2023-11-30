c7e67f4437dc8f85b5201c48448a0b0906f2f5a6
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-11-30 13:32:41 +0800

    Update and rename apowersoft.js to ApowersoftProCrack.js

--- ApowersoftProCrack.js
+++ ApowersoftProCrack.js
@@ -1,23 +1,23 @@
 /***********************************
 
 > 应用名称：傲软抠图
-> 软件版本：1.6.5
+> 软件版本：1.7.1
 > 下载地址：https://apps.apple.com/cn/app/id1490054676
-> 脚本作者：Cuttlefish
+> 脚本作者：ddgksf2013
 > 微信账号：墨鱼手记
-> 更新时间：2023-03-19
+> 更新时间：2023-11-30
 > 通知频道：https://t.me/ddgksf2021
 > 问题反馈：https://t.me/ddgksf2013_bot
 > 特别说明：本脚本仅供学习交流使用，禁止转载售卖
  
 [rewrite_local]
 
-# ～ 傲软抠图解锁会员权限（2023-03-19）@ddgksf2013
-https://awvp.aoscdn.com/base/vip/v2/vips url script-response-body apowersoft.js
+# ～ 傲软抠图解锁会员权限（2023-11-30）@ddgksf2013
+^https?:\/\/.*aoscdn\.com\/base\/vip\/v\d\/vips url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/ApowersoftProCrack.js
 
 [mitm] 
 
-hostname=awvp.aoscdn.com
+hostname=*.aoscdn.com
 
 ***********************************/
 
